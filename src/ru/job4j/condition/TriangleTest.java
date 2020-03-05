1e033f342a07dbc551fec76ff4c1ad06165e9b2f
RinZ26 <ivs1996@mail.ru>
2020-03-05 12:38:03 +0500

    0. Операции сравнения[#248266]

---
+++ src/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+import org.hamcrest.Factory;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+    @Test
+    public void whenExist() {
+        boolean actual = Triangle.exist(2.0, 2.0, 2.0);
+        boolean expected = true;
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenDoesntExist(){
+        boolean actual = Triangle.exist(3,3,10);
+        boolean expected = false;
+        assertThat(actual, is(expected));
+    }
+}
