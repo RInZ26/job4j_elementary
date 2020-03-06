40bc990e067cc3d940e311303374c65fd015c349
RinZ26 <ivs1996@mail.ru>
2020-03-06 13:46:38 +0500

    Исправления PrimeNumber

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int countOfPrimeNumbers(int number){
         int countOfPrimeNumbers = 0;
-            for (int i = 0; i < number + 1; i++)
+            for (int i = 2; i < number + 1; i++)
                 if (CheckPrimeNumber.check(i))
                     countOfPrimeNumbers++;
         return countOfPrimeNumbers;
