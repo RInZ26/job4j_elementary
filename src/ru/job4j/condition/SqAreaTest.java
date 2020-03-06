9b27345d8ef1f6b5ff5a663db071b8a6b9ef6129
RinZ26 <ivs1996@mail.ru>
2020-03-06 10:22:48 +0500

    Убираем плохие тесты!

--- src/ru/job4j/condition/SqAreaTest.java
+++ src/ru/job4j/condition/SqAreaTest.java
@@ -3,14 +3,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SqAreaTest {
-    @Test
-    public void square1(){
-        int p = -50;
-        int k = 10;
-        double expected = SqArea.square(p, k);
-        double actual = 0; //не хватает проверки на p > 0 в самом методе SqArea.square
-        Assert.assertEquals(expected, actual, 0.1);
-    }
 
     @Test
     public void square2(){
