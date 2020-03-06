5776c9d305e776a8511bb004a817f7cb51d013bb
RinZ26 <ivs1996@mail.ru>
2020-03-06 12:08:57 +0500

    5.7. Ипотека.[#248343]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int getCountOfYears(int amount, int salary, double percent){
+        int countOfYears = 0;
+        double debt = amount;
+            while (debt > 0){
+                debt = debt + debt * percent / 100 - salary;
+                    if(debt >= amount)
+                        return 999; // бесконечный долг ;(
+                countOfYears++;
+            }
+        return countOfYears;
+    }
+}
---
+++ src/ru/job4j/loop/MortgageTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MortgageTest {
+    @Test
+    public void when1Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.getCountOfYears(1000, 1200, 1);
+        assertThat(year, is(1));
+    }
+
+    @Test
+    public void when2Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.getCountOfYears(100, 120, 50);
+        assertThat(year, is(2));
+    }
+
+    @Test
+    public void whenPercentIsInsane(){
+        Mortgage mortgage = new Mortgage();
+        int expected = 999;
+        int actual = mortgage.getCountOfYears(100, 100, 100);
+        assertThat(actual, is(expected));
+    }
+}
