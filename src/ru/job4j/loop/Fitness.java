04282f0701abaab81d968dd42ae620a1a7f70076
RinZ26 <ivs1996@mail.ru>
2020-03-06 11:29:47 +0500

    5.4. Протеиновая диета.[#248341]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calcCountOfMonths(int ivan, int nic){
+        int countOfMonths = 0;
+            while (ivan <= nic){
+                ivan *= 3;
+                nic *= 2;
+                countOfMonths ++;
+            }
+        return countOfMonths;
+    }
+}
---
+++ src/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+
+public class FitnessTest {
+        @Test
+        public void calcCountofMonthsSimple() {
+            int expected = 1;
+            int actual = Fitness.calcCountOfMonths(90, 100);
+            Assert.assertThat(actual, Is.is(expected));
+        }
+        @Test
+        public void whenIvanGreatNik() {
+            int month = Fitness.calcCountOfMonths(95, 90);
+            Assert.assertThat(month, Is.is(0));
+        }
+
+        @Test
+        public void whenIvanLessByOneNik() {
+            int month = Fitness.calcCountOfMonths(90, 95);
+            Assert.assertThat(month, Is.is(1));
+        }
+
+        @Test
+        public void whenIvanLessByFewNik() {
+            int month = Fitness.calcCountOfMonths(50, 90);
+            Assert.assertThat(month, Is.is(2));
+        }
+}
+
