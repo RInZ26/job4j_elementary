0f6194fc91840e5a51cb6adf249e36653edbb65f
RinZ26 <ivs1996@mail.ru>
2020-03-06 13:40:28 +0500

    6.0.1. Объявление массива.[#248297]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+/**
+ * @Class Класс для определения массивов!
+ * @author RinZ26
+ * @since 06.03.2020
+ * @version 1
+ * */
+public class ArrayDefinition {
+    /**
+     * Создание различных массивов
+     * @param args -  args
+     * */
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
