bccd5b393fe4678e3317153b4b75bfb446c12ba0
RinZ26 <ivs1996@mail.ru>
2020-03-06 11:52:21 +0500

    5.6. Простые числа.[#248342]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int countOfPrimeNumbers(int number){
+        int countOfPrimeNumbers = 0;
+            for (int i = 0; i < number + 1; i++)
+                if (CheckPrimeNumber.check(i))
+                    countOfPrimeNumbers++;
+        return countOfPrimeNumbers;
+    }
+}
