6df60de15ff83444e73511f07c2e6028fe66b774
RinZ26 <ivs1996@mail.ru>
2020-03-06 12:32:24 +0500

    5.8. JavaDoc. Code convention[#248351]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Класс для вычисления арифметических операций + - * / ^.
+ * @author RinZ26
+ * @since 06.03.2020
+ * @version 1
+ */
 public class Calculator {
-
+    /**
+     * Main
+     * @param args - args
+     */
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
@@ -10,5 +19,15 @@ public class Calculator {
         one = 10;
         two = 11;
         System.out.println(result);
+    }
+
+    /**
+     * Сложение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * @return результат
+    */
+    public static double add (double first, double second){
+        return first + second;
     }
 }
---
+++ src/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Package for calculate task.
+ *
+ * @author Ivan Skvorchuk (ivs1996@mail.ru)
+ * @version 1
+ * @since 06.03.2020
+ */
+package ru.job4j.calculator;
