2fc4edac0f0840f7eef713ded5e8768135f29f29
RinZ26 <ivs1996@mail.ru>
2020-03-10 13:48:53 +0500

    6.8. Дефрагментация массива.[#248296]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * Клаcc для работы с массивами
+ * @author RinZ26
+ * @since 10.03.2020
+ * @version 1
+ */
+public class Defragment {
+    /**
+     * Дефрагментация массива
+     * @param array  исходный массив
+     * @return скомпресованный массив
+     */
+    public static String[] compress(String[] array) {
+        int pointer = -1; // указатель на последнюю ячейку со значением
+            for (int c = 0; c < array.length; c++)
+                if (array[c] != null)
+                    array[++pointer] = array[c];
+            if (pointer > -1)
+                for (int c = pointer + 1; c < array.length; c++)
+                    array[c] = null;
+        return array;
+    }
+}
