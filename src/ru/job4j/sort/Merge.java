3b1f694384ee8384791b5d8737835092cebfd025
RinZ26 <ivs1996@mail.ru>
2020-03-11 18:06:28 +0500

    1. Экзамен. Базовый синтаксис.[#248336]

---
+++ src/ru/job4j/sort/Merge.java
@@ -0,0 +1,45 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+/**
+ * Класс для работы с массивами
+ * @author RinZ26
+ * @since 11.03.2020
+ * @version 1
+ */
+public class Merge {
+    /**
+     * Слияние отсортированных массивов без потери их сортировки
+     * @param left  первый массив
+     * @param right  второй массив
+     * @return всеобъемлющий массив
+     */
+    public int[] merge(int[] left, int[] right) {
+        int[] array = new int[left.length + right.length];
+        int pointerLeft = 0, pointerRight = 0; // указатель на следующую ячейку
+            while (pointerLeft + pointerRight < array.length) {
+                if (pointerLeft < left.length && pointerRight < right.length) {
+                    array[pointerLeft + pointerRight] = left[pointerLeft] <= right[pointerRight] ? left[pointerLeft++] : right[pointerRight++];
+                } else if (pointerLeft >= left.length && pointerRight < right.length) {
+                    for (int i = pointerLeft  + pointerRight; i < array.length; i++) {
+                        array[i] = right[pointerRight++];
+                    }
+                } else if (pointerRight >= right.length && pointerLeft < left.length) {
+                    for (int i = pointerRight + pointerLeft; i < array.length; i++) {
+                        array[i] = right[pointerLeft++];
+                    }
+                }
+            }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
