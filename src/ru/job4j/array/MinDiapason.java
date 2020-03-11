cd6ceadf4de905624858806e8642ab5663f7f83f
RinZ26 <ivs1996@mail.ru>
2020-03-11 10:35:28 +0500

    Исправления в  6.5.2. Поиск минимума в диапазо.[#248285]

--- src/ru/job4j/array/MinDiapason.java
+++ src/ru/job4j/array/MinDiapason.java
@@ -4,7 +4,7 @@ package ru.job4j.array;
  * Поиск минмимума в диапазоне !!!
  * @author RinZ26
  * @since 10.03.2020
- * @version 1
+ * @version 2
  */
 public class MinDiapason {
     /**
@@ -12,12 +12,9 @@ public class MinDiapason {
      * @param array заданный массив
      * @param start начало диапазона
      * @param finish конец диапазона
-     * @return минимум, -1 - значения границ некорректны
+     * @return минимум
      */
     public static int findMin(int[] array, int start, int finish) {
-            if (finish < start || start > array.length || finish > array.length || start < 0 || finish < 0) {
-                return -1;
-            }
         int min = array[start];
             for (int i = start + 1; i <= finish; i++) {
                 if (min > array[i]) {
